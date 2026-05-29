56b12d55ed6f721d666ac5212f9d901a1d2e322d
Owen Jones <owen@stackhpc.com>
2026-05-29 14:37:53 +0100

    CI: Fix testing
    * Add sanity ignores for newer Ansible versions (2.19 and 2.20)
    * Add `checks:write` permission for ansible testing job
    * Update module argument setting method to use Ansible's
      `patch_module_args` helper

--- tests/unit/modules/cephadm_test_common.py
+++ tests/unit/modules/cephadm_test_common.py
@@ -1,16 +1,16 @@
-from ansible.module_utils import basic
-from ansible.module_utils._text import to_bytes
-import json
+from ansible.module_utils.testing import patch_module_args
+import contextlib
 
 
+@contextlib.contextmanager
 def set_module_args(args):
     if '_ansible_remote_tmp' not in args:
         args['_ansible_remote_tmp'] = '/tmp'
     if '_ansible_keep_remote_files' not in args:
         args['_ansible_keep_remote_files'] = False
 
-    args = json.dumps({'ANSIBLE_MODULE_ARGS': args})
-    basic._ANSIBLE_ARGS = to_bytes(args)
+    with patch_module_args(args):
+        yield
 
 
 class AnsibleExitJson(Exception):
